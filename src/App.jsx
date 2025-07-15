dce9ed93ce00f0adac306eb77d4c7ffa7fe36235
MD Monirujjaman <mdmonirujjaman@MDs-MacBook-Air.local>
2025-07-15 19:04:33 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='bg-amber-800 text-center text-4xl font-bold text-white'>Hello World From React......</h1>
+    </>
+  )
+}
+
+export default App
